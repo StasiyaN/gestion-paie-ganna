9d8a1052b8fc041ca1fc22a4017696163c812542
nyzana <nyzana.dev@gmail.com>
2024-10-04 15:03:28 +0200

    footer removed from all the pages except index + js footer charge removed + base for rh page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   console.log ('main script ok');
 
-// Function to load the header and footer
-function includeHTML() {
-    let elements = document.querySelectorAll('[data-include]');
-    
-    elements.forEach(function(el) {
-      let file = el.getAttribute('data-include');
-      
-      fetch(file)
-        .then(response => response.text())
-        .then(data => {
-          el.innerHTML = data;
-        })
-        .catch(err => console.error('Error loading file: ', err));
-    });
-  }
-  
-  // Call the function after DOM is loaded
-  window.onload = includeHTML;
-
   //show more other experience
 // Get the elements
 const toggleBtn = document.getElementById('toggle-btn');
